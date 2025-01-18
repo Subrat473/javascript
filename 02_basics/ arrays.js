74ba8901b1768fc4bbe05252123f0aa362058fa8
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-01-18 09:45:01 +0000

    array

---
+++ 02_basics/ arrays.js
@@ -0,0 +1,22 @@
+// array
+
+
+const myArr = [0,1,2,3,4,5,6]
+const myHeros = ["shaktiman","goga"]
+                         
+const myArr2 = new Array (1, 2, 3, 4)
+
+
+
+console.log(myArr [1]);
+
+
+
+
+
+/// Array methods 
+
+
+myArr.push(6)
+console.log(myArr);
+          
