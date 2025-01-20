942c8ff2806bb5a2dda3c33d9cdef1447e3523da
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-01-20 03:44:21 +0000

    scope

---
+++ 04_scope/nasted_scopes.js
@@ -0,0 +1,44 @@
+function one () {
+    const username = " subrat "
+
+    function two () {
+        const website = " youtube "
+        console.log(username);
+        
+    }
+    //console.log(website);
+    
+}
+//one()
+
+
+if (true) {
+    const username = " subrat "
+    if (username === " subrat "){
+        const website = " youtube "
+        //console.log(username + website);
+        
+    }
+   // console.log(website);
+    
+}
+console.log(username);
+
+
+
+
+
+//+++++++++++++++++++++ interesting +++++++++++++++++++++
+
+
+
+function addone ( num ) {
+    return num + 1 
+}
+addone (5)
+
+
+const addtwo = function(num){
+    return num + 2 
+}
+addtwo(5)
---
+++ 04_scope/scopes.js
@@ -0,0 +1,13 @@
+//var c = 3000
+//var = 30
+let a = 300
+
+if (true){
+    let a = 10
+    const b = 20
+    console.log("INNER", a )
+}
+
+console.log(a);
+//console.log(b);
+//console.log(c);
