c52fa2c2c6801d7a955a2f99edc5c657bcffe90b
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-01-20 09:08:55 +0000

    arrow

---
+++ 05_arrow/arrow.js
@@ -0,0 +1,35 @@
+const user = {
+    username : "subrat",
+    price : 150,
+    
+    welcomeMessage: function () {
+        //console.log('${this.username} , welcome to website');
+        //console.log(this);
+        
+
+    }
+
+}
+
+//user.welcomeMessage()
+//user.username = " sam "
+//user.welcomeMessage()
+
+
+
+//function chai () {
+   // let username = " subrat "
+   // console.log(this);
+    
+}
+//chai()
+
+//const chai = function () =>  { 
+//    let username = " subrat "
+ //   console.log(this);
+//}
+//chai
+
+const addTwo = ( num1, num2) => (num1 + num2)
+
+console.log(addTwo(5+5));
