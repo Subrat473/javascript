f0418bce9bbf3f642be8a8563eafd213addab8f1
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-02-11 12:27:00 +0000

    add

--- important_questions/functions.js
+++ important_questions/functions.js
@@ -12,3 +12,32 @@ function map(f, a) {
   }, numbers);
   console.log(cubedNumbers); // [0, 1, 8, 125, 1000]
   
+
+  // another 
+
+
+  // The following variables are defined in the global scope
+const num1 = 20;
+const num2 = 3;
+const name = "Chamakh";
+
+// This function is defined in the global scope
+function multiply() {
+  return num1 * num2;
+}
+
+console.log(multiply()); // 60
+
+// A nested function example
+function getScore() {
+  const num1 = 2;
+  const num2 = 3;
+
+  function add() {
+    return `${name} scored ${num1 + num2}`;
+  }
+
+  return add();
+}
+
+console.log(getScore()); // "Chamakh scored 5"
