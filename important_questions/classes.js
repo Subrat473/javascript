f0b3131b336ab6731dc84533d90511393d838621
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-02-19 17:57:45 +0000

    classes

---
+++ important_questions/classes.js
@@ -0,0 +1,26 @@
+class MyClass {
+    // Constructor
+    constructor() {
+      // Constructor body
+    }
+    // Instance field
+    myField = "foo";
+    // Instance method
+    myMethod() {
+      // myMethod body
+    }
+    // Static field
+    static myStaticField = "bar";
+    // Static method
+    static myStaticMethod() {
+      // myStaticMethod body
+    }
+    // Static block
+    static {
+      // Static initialization code
+    }
+    // Fields, methods, static fields, and static methods all have
+    // "private" forms
+    #myPrivateField = "bar";
+  }
+  
