a039f09f2d2248a447612cd142f8e76476de84eb
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-03-07 11:45:48 +0000

    1st_Questions

---
+++ Interview_Prep/Easy_questions.js
@@ -0,0 +1,60 @@
+// Variables and Data Types (10 Questions )
+
+// Declare a variable using let and log its value.
+
+let a = 12;
+console.log(a);
+
+// Create a constant to store the value of PI and log it.
+
+const pi = Math.PI;
+console.log(pi);
+
+
+// Reassign a value to a variable declared with let and log the result.
+
+let a = 12;
+a = 32;
+console.log(a)
+
+// Check the type of null and log it.
+
+log a = null;
+console.log(typeof a); 
+
+
+// Create a variable with a number as a string (e.g., "25") and log its type/
+
+ var a = "254";
+ console.log(typeof a);
+
+// Use typeof to check the type of a boolean variable.
+
+
+var a = true;
+console.log(typeof a);
+
+
+// Create three variables of types string, number and boolen, and log their values.
+
+let aa = "hello", bb = 12, cc=false;
+console.log(aa,bb,cc);
+
+// Declare a variable without assigning a value. Log it's type.
+
+var a;
+console.log(typeof a);
+
+// Create a variable with undefined with undefined and log its type.
+
+const a = [1,2,3,4];
+a.pop();
+
+
+
+
+
+// Use const to create and array. Try reassigning the array and observe the error.
+
+const myArray = [1, 2, 3];
+myArray = [4, 5, 6]; // This will throw an error
---
+++ Interview_Prep/Most_Easy_Questions.js
@@ -0,0 +1,66 @@
+// 1st Console Usage (10 Qusestion)
+
+
+//  Log your Name 
+
+console.log("Subrat", "Travel would be");
+
+
+// Perform and log the result of 45*2 - 10 
+
+console.log(45*2-10);
+
+
+
+// Use console.log() to display the current year.
+
+const newdate = new Date();
+console.log(newdate.getFullYear());
+
+
+
+// Crate two variables for first and last name. Concatenate and log them.
+
+var first = "harsh";
+var last = "sharma";
+
+console.log(`${first} ${last}`);
+
+
+
+// Track the value of a variable by logging it before and after updating.
+
+var a = 12;
+console.log(a);
+a = 32;
+console.log(a);
+
+
+
+// Use console.error() to simulate an error message.
+
+console.error("Something went wrong");
+
+
+
+// Log the square of the number 112 to the console.
+
+
+console.log(12*12);
+
+// Print the type of a variable holding the value true.
+
+var a = true;
+console.log(typeof a);
+
+// Create a variable holding your age and log whether it's grater than 18.
+
+var age = 22;
+if (age > 18 ) console.log(true);
+else console.log(false);
+
+
+// Log the result of 100/0 and observe the output
+
+console.log(100/0);
+ 
